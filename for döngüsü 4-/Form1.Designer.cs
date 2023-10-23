382f6c155f630120ad50d00a9bc979b0764aa100
Iremkeser37 <145453393+Iremkeser37@users.noreply.github.com>
2023-10-23 14:24:06 +0300

    Add files via upload

---
+++ for döngüsü 4-/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace for_döngüsü_4_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSayılar = new System.Windows.Forms.ListBox();
+            this.txtBaslangic = new System.Windows.Forms.TextBox();
+            this.txtBitis = new System.Windows.Forms.TextBox();
+            this.btnYaz = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbSayılar
+            // 
+            this.lbSayılar.FormattingEnabled = true;
+            this.lbSayılar.Location = new System.Drawing.Point(188, 59);
+            this.lbSayılar.Name = "lbSayılar";
+            this.lbSayılar.Size = new System.Drawing.Size(149, 264);
+            this.lbSayılar.TabIndex = 0;
+            // 
+            // txtBaslangic
+            // 
+            this.txtBaslangic.Location = new System.Drawing.Point(27, 98);
+            this.txtBaslangic.Name = "txtBaslangic";
+            this.txtBaslangic.Size = new System.Drawing.Size(118, 20);
+            this.txtBaslangic.TabIndex = 1;
+            // 
+            // txtBitis
+            // 
+            this.txtBitis.Location = new System.Drawing.Point(27, 155);
+            this.txtBitis.Name = "txtBitis";
+            this.txtBitis.Size = new System.Drawing.Size(118, 20);
+            this.txtBitis.TabIndex = 2;
+            // 
+            // btnYaz
+            // 
+            this.btnYaz.Location = new System.Drawing.Point(43, 222);
+            this.btnYaz.Name = "btnYaz";
+            this.btnYaz.Size = new System.Drawing.Size(102, 60);
+            this.btnYaz.TabIndex = 3;
+            this.btnYaz.Text = "YAZ";
+            this.btnYaz.UseVisualStyleBackColor = true;
+            this.btnYaz.Click += new System.EventHandler(this.btnYaz_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnYaz);
+            this.Controls.Add(this.txtBitis);
+            this.Controls.Add(this.txtBaslangic);
+            this.Controls.Add(this.lbSayılar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbSayılar;
+        private System.Windows.Forms.TextBox txtBaslangic;
+        private System.Windows.Forms.TextBox txtBitis;
+        private System.Windows.Forms.Button btnYaz;
+    }
+}
+
---
+++ for döngüsü 4-/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_döngüsü_4_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYaz_Click(object sender, EventArgs e)
+        {
+            lbSayılar.Items.Clear();
+
+            int bitis = Convert.ToInt32(txtBitis.Text);
+            int baslangic = Convert.ToInt32(txtBaslangic.Text);
+
+            for (int i = baslangic; i <= bitis; i++  )
+            {
+                lbSayılar.Items.Add(i);
+            }
+            
+
+            
+        }
+    }
+}
